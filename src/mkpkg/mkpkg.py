ce4a7da10f1b97376f957be7796d5ab13e309657
Gene C <arch@sapience.com>
2022-10-13 15:21:10 -0400

    Clean up some comments

--- src/mkpkg/mkpkg.py
+++ src/mkpkg/mkpkg.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 """
- Arch tool to ensure packages are rebuilt when some dependencies are newer than the last build
- Uses makepkg for actual rebuilds.
+ Tool for Arch PKGBUILD to ensure packages are rebuilt when some dependencies requirements 
+ trigger a rebuild. 
+ NB Uses makepkg for actual builds.
 
  Uses 2 new PKGBUILD variables to define rebuild triggers
-
  See README for more details
 
  gene Oct 2022
@@ -16,10 +16,6 @@ from utils import MkPkg
 def main():
     """
     Build package if trigger dependencies have changed or as usual if package itself has changed.
-        mkpkg_newer - if any package listed was installed since the last build
-        mkpkg_vers  - version constraints. Uses semantic versioning.
-                      Can trigger on any version, major, major.minor or major.minor.patch
-    Uses regular makepkg to do the actual build
     """
     #pdb.set_trace()
 
