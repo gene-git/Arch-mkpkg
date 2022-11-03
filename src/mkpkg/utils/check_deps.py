8966a24439226c2829e13c08ebaf2e2ef9bb1e1b
Gene C <arch@sapience.com>
2022-11-03 14:34:23 -0400

    bug fix incorrectly handling triggers pkg>xxx

--- src/mkpkg/utils/check_deps.py
+++ src/mkpkg/utils/check_deps.py
@@ -48,7 +48,7 @@ def check_deps(mkpkg):
     #
     # if no deps then nothing to do
     #
-    if not (mkpkg.depends or mkpkg.depends_files):
+    if not (mkpkg.depends or mkpkg.depends_files or mkpkg.depends_vers):
         return (False, False)
 
     #
--- src/mkpkg/utils/split_deps.py
+++ src/mkpkg/utils/split_deps.py
@@ -8,7 +8,7 @@
 # pylint: disable=R0912,R0915
 from .dep_vers import read_last_dep_vers
 
-def _split_pkg_vers_constraint(opers, item):
+def _split_pkg_vers_constraint(mkpkg, item):
     """
     Takes a string in form 'pkgname oper vers'
         where oper is one of the opers list
@@ -18,19 +18,41 @@ def _split_pkg_vers_constraint(opers, item):
     pkg = None
     oper = None
     vers_trigger = None
-    for oper_try in opers:
-        if oper_try in item:
-            oper = oper_try
-            isplit = item.split(oper)
-            pkg = isplit[0].strip()
-            if len(isplit) > 1:
-                if '>=' in isplit:
-                    vsplit = isplit[1].split('=')
-                    if len(vsplit) > 1:
-                        vers_trigger = vsplit[1]
-                    else:
-                        vers_trigger = isplit[1].strip()
-            break
+    wmsg = mkpkg.wmsg
+
+    #
+    # add white space
+    #
+    xitem = item.replace('>', ' > ')
+    xitem = xitem.replace('<', ' < ')
+    xitem = xitem.replace('< =', '<= ')
+    xitem = xitem.replace('> =', '>= ')
+
+    splitem = xitem.split()
+    num_items = len(splitem)
+    if num_items == 1:
+        pkg = splitem[0]
+
+    elif num_items == 2:
+        # incomplete either a) (pkg oper) or (pkg vers)
+        # we can return orig or first piece as it makes no sense
+        # we return orig
+        pkg = item.strip()
+        wmsg(f'Error _mkpkg_depends item should be "X" or "X>Y" etc. Got: {item}')
+
+    elif num_items == 3:
+        pkg = splitem[0]
+        oper = splitem[1]
+        vers_trigger = splitem[2]
+
+    else:
+        # ug - also makes no sense
+        pkg = item.strip()
+        wmsg(f'Error _mkpkg_depends item should be "X" or "X>Y" etc. Got: {item}')
+
+    return (pkg, oper, vers_trigger)
+
+
 
     return (pkg, oper, vers_trigger)
 
@@ -47,7 +69,7 @@ def split_deps_vers_list(mkpkg):
     dep_vers = []
     opers = mkpkg.dep_vers_opers
     for item in mkpkg.depends:
-        (pkg, oper, vers_trigger) = _split_pkg_vers_constraint(opers, item)
+        (pkg, oper, vers_trigger) = _split_pkg_vers_constraint(mkpkg, item)
         if oper:
             this = (pkg, oper, vers_trigger)
             dep_vers.append(this)
