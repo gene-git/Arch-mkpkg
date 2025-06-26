c502fd80da0028b3b562879be770481b6cc69a28
Gene C <arch@sapience.com>
2025-06-26 09:01:25 -0400

    run_prog: sync local copy with latest from pyconcurrent

--- Docs/conf.py
+++ Docs/conf.py
@@ -9,7 +9,7 @@
 project = "mkpkg"
 copyright = '2023, Gene C'
 author = 'Gene C'
-release = '7.6.0'
+release = '7.7.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
